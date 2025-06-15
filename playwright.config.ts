15c61e361adb4a22b6040441cfe749ab3c991775
AQR88 <timahaua@gmail.com>
2025-06-15 20:27:59 +0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://localhost:3000',
-    headless: false,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
     screenshot: "only-on-failure",
@@ -38,7 +37,7 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"], headless: false },
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
